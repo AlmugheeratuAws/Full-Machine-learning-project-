b99ac099465b27c5d0fdfca73614989c8a08d7d9
AlmugheeratuAws <ma.aalhujayr@gmail.com>
2026-02-11 15:53:27 +0300

    Update DecisionTreeClassifier.py

--- Models/DecisionTreeClassifier.py
+++ Models/DecisionTreeClassifier.py
@@ -12,3 +12,13 @@ Decision_class_report=classification_report(y_test, y_pred2)
 print("Accuracy Score: ", Decision_acc_score)
 print("Confusion Matrix: ", Decision_conf_matrix)
 print("Classification Report: ", Decision_class_report)
+
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+# Assuming Decision_model is your trained Decision Tree Classifier model
+# Assuming x_train are your training features
+
+plt.figure(figsize=(20,10))
+plot_tree(Decision_model, feature_names=x_train.columns, filled=True, class_names=['<=50K', '>50K'])
+plt.show()
