d3c2ad83d6da3a5d18f1d41b9f3ddd0d4b4c4f85
AlmugheeratuAws <ma.aalhujayr@gmail.com>
2026-02-11 15:50:58 +0300

    Update LogisticRegression.py

--- Models/LogisticRegression.py
+++ Models/LogisticRegression.py
@@ -4,3 +4,11 @@ logistic_model.fit(x_train, y_train)
 
 y_pred=logistic_model.predict(x_test)
 print(y_pred)
+
+# Evaluate the model
+Logistic_acc_score=accuracy_score(y_test, y_pred)
+Logistic_conf_matrix=confusion_matrix(y_test, y_pred)
+Logistic_class_report=classification_report(y_test, y_pred)
+print("Accuracy Score: ", Logistic_acc_score)
+print("Confusion Matrix: ", Logistic_conf_matrix)
+print("Classification Report: ", Logistic_class_report)
